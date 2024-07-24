3baf0b0b26be324282ecbedd87a13a30c104a9db
jaiswalaman2020 <jaiswalaman2020@gmail.com>
2024-07-24 22:27:43 +0530

    added share page

---
+++ components/meals/image-picker.js
@@ -0,0 +1,57 @@
+"use client";
+
+import { useRef } from "react";
+import { useState } from "react";
+import Image from "next/image";
+
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setSelectedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!selectedImage && <p>No image picked yet.</p>}
+          {selectedImage && (
+            <Image src={selectedImage} alt="Picked Image" fill />
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
